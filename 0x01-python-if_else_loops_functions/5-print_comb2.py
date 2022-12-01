dbdad844e59727632c01c1995f9780cc2025069e
osikoyaop <optimisticopyso@gmail.com>
2022-11-30 18:35:45 -0800

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print(i)
+        print("{0}".format(i))
     else:
-        print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
+        print("{0:02}".format(i), end=', ')
